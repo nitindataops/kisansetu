4344cb741dc06fb6eb602ab3ebd155936c216912
nitindataops <nk565556@gmail.com>
2026-09-08 00:13:41 +0530

    Use Render PORT environment variable

--- server.ts
+++ server.ts
@@ -141,7 +141,7 @@ import {
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ extended: true, limit: '20mb' }));
